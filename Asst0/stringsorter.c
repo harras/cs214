90f176add6babb7950edb5ffc37de7c9b3107ece
blackadder <nickharras1@gmail.com>
2018-01-21 04:32:52 -0500

    Adding an alphabetic char scanner

--- Asst0/stringsorter.c
+++ Asst0/stringsorter.c
@@ -2,9 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+int is_alpha(char c){
+    if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') )
+	    return 1;
+	else
+		return 0;
+}
+
 int main(int argc, char *argv[]) {
 	//Catches invalid inputs and prints the input format
-	if(agrc > 2){
+	if(argc != 2){
 	    printf("Program must take one string parameter. Like so:\n");
 		printf("./stringsorter \"Example string here\"\n");
 		exit(1);
@@ -12,12 +19,16 @@ int main(int argc, char *argv[]) {
 
     //Copies the value at argv to variable s
 	char s[strlen(argv[1])];
-    char *iter = *s;
+    char *iter;
+	iter = s;
 	strcpy(s, argv[1]);
 
 	while(*iter != '\0'){
-		while(/*Non-letter chars*/){
+		while(!is_alpha(*iter)){
+			*iter = ' ';			
+			iter++;
 		}
+		iter++;
 	}
 
     printf("%s\n", s);
