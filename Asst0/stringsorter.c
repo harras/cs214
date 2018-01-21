805a4ec56a85e65d64a7ad7b4251e9d89fae0a09
blackadder <nickharras1@gmail.com>
2018-01-21 04:14:42 -0500

    Adding various things... messing around...

--- Asst0/stringsorter.c
+++ Asst0/stringsorter.c
@@ -3,8 +3,23 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-    char s[strlen(argv[1])];
-    strcpy(s, argv[1]);
-    
+	//Catches invalid inputs and prints the input format
+	if(agrc > 2){
+	    printf("Program must take one string parameter. Like so:\n");
+		printf("./stringsorter \"Example string here\"\n");
+		exit(1);
+	}
+
+    //Copies the value at argv to variable s
+	char s[strlen(argv[1])];
+    char *iter = *s;
+	strcpy(s, argv[1]);
+
+	while(*iter != '\0'){
+		while(/*Non-letter chars*/){
+		}
+	}
+
     printf("%s\n", s);
+	exit(0);
 }
