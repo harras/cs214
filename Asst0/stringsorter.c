80f0229d39886091a0881389ab69c3d5acec5c19
Nicholas Harras <nickharras1@gmail.com>
2018-01-30 22:23:08 -0500

    Adding documentation

--- Asst0/stringsorter.c
+++ Asst0/stringsorter.c
@@ -8,13 +8,9 @@ struct Node {
 	struct Node *next;
 };
 
+// Global variable, head pointer.
+// Points to the first node in the sorted LL.
 struct Node *head = NULL;
-
-char * lower_all(char *str){
-	for(int i = 0; str[i]; i++){
-  		str[i] = tolower(str[i]);
-	}
-}
 
 // Returns 1 if a string goes before -- or is the same string as -- another alphabetically.
 // BUG: 'hello, hell' returns a 1. 
@@ -37,27 +33,32 @@ int goes_before(char *str1, char *str2){
 	return 1;
 }
 
+// Inserts words in alphabetical order into a linked list using insertion sort.
 void insert(char *word){
 
 	struct Node *new_node = (struct Node *) malloc(sizeof(struct Node));
 	struct Node *temp = head;
 
+	// New string
 	new_node->str = malloc(strlen(word) + 1);
 	strcpy(new_node->str, word);
 	
 	new_node->next = NULL;
 
+	// Create a head node if none exists
 	if (head == NULL){
 		head = new_node;
 		return;
 	}
 
+	// Replace the head node if the new string should be place at the front of the list
 	if(goes_before(new_node->str, head->str)){
 		new_node->next = head;
 		head = new_node;
 		return;
 	}
 
+	// Main loop, iterate through LL until the word's alphabetic location is found
 	while(temp->next != NULL){
 		if(!goes_before(new_node->str, temp->str) && 
 			(goes_before(new_node->str, temp->next->str))){
@@ -68,12 +69,14 @@ void insert(char *word){
 		temp = temp->next;
 	}
 
+	// Add word to the end of the LL if it should be placed after the last word in LL
 	if(goes_before(temp->str, new_node->str)){
 		temp->next = new_node;
 		return;
 	}
 }
 
+// Takes global head pointer, from there prints entire LL
 void print_list(){
 	struct Node *n = head;
 
@@ -84,6 +87,7 @@ void print_list(){
 }
 
 int main(int argc, char *argv[]) {
+
 	// Catches invalid inputs and prints the input format
 	if(argc != 2){
 	    printf("Program must take one string parameter. Like so:\n");
@@ -99,7 +103,7 @@ int main(int argc, char *argv[]) {
 	end = argv[1];
 
 	// Main loop of the program, iterates over the input string and sorts each scanned word
-	// NOTE TO SELF: Revise this, it's clunky
+	// Adds scanned word into linked list using insertion sort
 	while(*end != '\0'){
 		while(!isalpha(*end) && *end != '\0'){
 			if(isalpha(*front)){
@@ -115,6 +119,7 @@ int main(int argc, char *argv[]) {
 			end++;
 		}
 	}
+	// Catches final word at the end of the input string
 	if(isalpha(*front)){
 		memset(word, '\0', sizeof(word));
 		memcpy(word, front, (end-front));
