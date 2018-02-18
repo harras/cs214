1b95ed408e7b73ed58da06c4e403fa6eb5f6d29d
harras <nickharras1@gmail.com>
2018-02-18 18:52:05 -0500

    Updating my version

--- Asst1/memgrind.c
+++ Asst1/memgrind.c
@@ -2,90 +2,119 @@
 
 
 int main(int argc, char* argv[]){
-	int i;
-	struct timeval start, end;
+	int i,x;
+	struct timeval start;
+	struct timeval end;
 	double elapsed;
+	char * ptrs[150];
 	srand(time(NULL));
 
 
 	// A
 	gettimeofday(&start, NULL);
-	for(i = 0; i < 150; i++){
-		void * ptr = mymalloc(1, __FILE__, __LINE__);
-		myfree(ptr, __FILE__, __LINE__);
+	for(x=0;x<100;x++){
+		for(i = 0; i < 150; i++){
+			void * ptr = malloc(1);
+			free(ptr);
+		}
 	}
 	gettimeofday(&end, NULL);
-	elapsed = (end.tv_sec - start.tv_sec) * 1000.0;
-	elapsed += (end.tv_usec - start.tv_usec) / 1000.0; // idk why but this is always 0.
-	printf("Test A...time elapsed:\t%fms\n", elapsed);
+	elapsed = (end.tv_usec - start.tv_usec);
+	printf("Test A...Average time per workload:\t%.0fus\n", elapsed/100);
 
 
 	// B
 	gettimeofday(&start, NULL);
-	void * ptrs[150];
-	for(i = 0; i < 150; i++){
-		//printf("malloc...%d...\n", i);
-		ptrs[i] = mymalloc(1, __FILE__, __LINE__);
-	}
-	for(i = 0; i < 150; i++){
-		//printf("free...%d...\n", i);
-		myfree(ptrs[i], __FILE__, __LINE__);
+	for(x=0;x<100;x++){
+		for(i = 0; i < 150; i++){
+			ptrs[i] = malloc(1);
+		}
+		for(i = 0; i < 150; i++){
+			free(ptrs[i]);
+		}
 	}
 	gettimeofday(&end, NULL);
-	elapsed = (end.tv_sec - start.tv_sec) * 1000.0;
-	elapsed += (end.tv_usec - start.tv_usec) / 1000.0; 
-	printf("Test B...time elapsed:\t%fms\n", elapsed);
+	elapsed = (end.tv_usec - start.tv_usec); 
+	printf("Test B...Average time per workload:\t%.0fus\n", elapsed/100);
 
 
 	// C
 	gettimeofday(&start, NULL);
-	int count = 0;
-	int malloc_count = 0;
-	while(malloc_count < 150){
-		int r = rand()%2;
-		if(r == 0){
-			ptrs[count++] = mymalloc(1, __FILE__, __LINE__);
-			malloc_count++;
-		}
-		else  if(r == 1 && count < 0){
-			myfree(ptrs[count--], __FILE__, __LINE__);
-		}
-		else{
-			myfree(ptrs[0], __FILE__, __LINE__);
-		}
-	} // will this work idk lol
+	for(x=0;x<100;x++){
+		int totalPointers = 0;
+		int numFreed=0;
+		int r=0;
+		while(numFreed<150){
+			r = rand()%2;
+			//if remainder is 0 and we havent hit the cap or there are no pointers to free, allocates another pointer
+			if((r == 0 && totalPointers<150) || numFreed==totalPointers){
+				ptrs[totalPointers] = malloc(1);
+				totalPointers++;
+			}
+			//remainder is 1 and there are more pointers allocated than freed
+			else{
+				free(ptrs[numFreed]);
+				numFreed++;
+			}
+		} 
+	}
 	gettimeofday(&end, NULL);
-	elapsed = (end.tv_sec - start.tv_sec) * 1000.0;
-	printf("Test C...time elapsed:\t%fms\n", elapsed);
+	elapsed = (end.tv_usec - start.tv_usec); 
+	printf("Test C...Average time per workload:\t%.0fus\n", elapsed/100);
 
-
-	// E
+	//D
 	gettimeofday(&start, NULL);
-	int * int_ptrs[100];
-	i = 0;
-	while(i < 100){
-		int_ptrs[i] = mymalloc(4, __FILE__, __LINE__);
-		i++;
-	}
-	i = 0;
-	while(i < 100){
-		int r = rand()%100+1;
-		int_ptrs[i] = &r;
-		i++;
-	}
-	i = 0;
-	while(i < 100){ // To be commented out in final version, an option to test things
-		printf("value at ptrs[%d]:\t%d\n", i, *int_ptrs[i]);
-		i++;	
-	}
-	i = 0;
-	while(i < 100){
-		myfree(int_ptrs[i], __FILE__, __LINE__);
-		i++;
+	for(x=0;x<100;x++){
+		int totalPointers = 0;
+		int numFreed=0;
+		int r=0;
+		//maxes out at 73 to guarantee the array won't fill (5000/68=73.5)
+		while(numFreed<150){
+			//printf("numFreed=%d\n",numFreed);
+			r = rand()%2;
+			//if remainder is 0 and we havent hit the cap or there are no pointers to free, allocates another pointer
+			if((r == 0 && totalPointers<100) || numFreed==totalPointers){
+				ptrs[totalPointers]=NULL;
+				while(ptrs[totalPointers]==NULL){
+					ptrs[totalPointers] = malloc((rand()%64)+1);
+				}
+				totalPointers++;
+			}
+			//remainder is 1 and there are more pointers allocated than freed
+			else{
+				free(ptrs[numFreed]);
+				numFreed++;
+			}
+		} 
 	}
 	gettimeofday(&end, NULL);
-	elapsed = (end.tv_sec - start.tv_sec) * 1000.0;
-	printf("Test C...time elapsed:\t%fms\n", elapsed);
+	elapsed = (end.tv_usec - start.tv_usec); 
 
-	return 0;
+	printf("Test D... Average time per workload:\t%.0fus\n", elapsed/100);
+
+	// E: demonstrates memory is useable
+	gettimeofday(&start, NULL);
+	for(x=0;x<100;x++){
+		int** grid=(int**) malloc(sizeof(int*)*15);
+		for(i=1;i<15;i++){
+			grid[i]=(int*) malloc(sizeof(int)*2);
+			grid[i][0]=i;
+			grid[i][1]=i;
+		}
+		for(i=1;i<15;i++){
+			grid[i][1]=i+1;
+		}
+		for(i=1;i<15;i++){
+			free(grid[i]);
+		}
+		free(grid);
+	}
+	gettimeofday(&end, NULL);
+	elapsed = (end.tv_usec - start.tv_usec);
+	printf("Test E... Average time per workload:\t%.0fus\n", elapsed/100); 
+	
+	// F: demonstrates the allocation of large blocks of memory
+
+
+    return 0;
 }
--- Asst1/mymalloc.c
+++ Asst1/mymalloc.c
@@ -2,118 +2,154 @@
  * for improved malloc and free functions
  *
  *
- * block headers are 4 byte linked lists with a 2 byte "next"
- * pointer (an offset from front of the block) and 2 byte memory
+ * block headers are 4 byte linked lists with a 2 byte "next" 
+ * pointer (an offset from front of the block) and 2 byte memory 
  * size field
- *
+ * 
  *  block structure:
  *       [][]                         [][]                            [][][]][][][][].....
  *  index of next header    # of mem bytes in this block             bytes of mem allocated
  **/
- 
- 
+
+
 //the amount of memory available before the next pointer: pointer-4+(next-memory)
 #include "mymalloc.h"
 static char myblock[5000];
- 
+
 // dummy function used to debug random things: to be removed before submitting
 void trythis(){
     char* blerptr=&myblock[0];
     long blerp=(long) blerptr;
     printf("\narray starts at %ld\n",blerp);
 }
- 
+
 // improvement on malloc
 // Returns a void* pointer for valid requests or "NULL" for invalid requests. Prints calling file and line of invalid request
 void* mymalloc(int memRequest, char* filename, int line){
+    //makes a short pointer to the same block for the purpose of grabbing headers
+    //myshortblock indices are worth 2 of myblock indices
+    unsigned short* myShortBlock=(unsigned short*) &myblock[0];
+    //converts memrequest to a short type, asks for half as many shorts as bytes
     unsigned short memReqShort=(unsigned short) memRequest;
+    //rounds memory request to an even number (tempPtr indices are equal to 2 bytes, headers can't start on odd indices
+    if(memReqShort%2==1)
+        memReqShort++;
+    memReqShort=memReqShort/2;
     // The 4 bytes at the 0 index are a dummy header with a pointer to the 4 index and 0 memory allocated
-    if((unsigned short) myblock[0]==0){
-        unsigned short firstblock=4;
-        myblock[0]=firstblock;
-        unsigned short blankmem=0;
-        myblock[2]=blankmem;
+    if(myShortBlock[0]==0){
+        myShortBlock[0]=2;
+        myShortBlock[1]=0;
     }
-   
-    // if the request exceeds maximum available memory, does not try to allocate
-    if((memReqShort+4)>5000){
-        printf("\nExcessive memory request error at %s, line %d", filename, line);
+    if(memRequest==0){
         return NULL;
     }
-   
+    
+    // if the request exceeds maximum available memory, does not try to allocate
+    if((memReqShort+2)>2500){
+        printf("\nExcessive memory request error at %s, line %d\n", filename, line);
+        return NULL;
+    }
+    
     // myblock indices used to iterate
     unsigned short currindex=0;
     unsigned short nextindex=0;
-   
+    
     // Finds first chunk of memory large enough
     // either gets to the end of allocated memory or there is sufficient free memory before the next to squeeze in a block
-    while(myblock[currindex]!=0 && ((unsigned short) myblock[currindex]-(currindex+4+memReqShort))<(memReqShort+4) ){
-        currindex=(unsigned short) myblock[currindex];
+    while(myShortBlock[currindex]!=0 && (myShortBlock[currindex]-(currindex+2+myShortBlock[currindex+1]))<(memReqShort+2) ){
+        currindex=myShortBlock[currindex];
     }
-   
+    
     // Case: "next" field is 0 because it is the last block of memory and the request couldn't be accomodated anywhere earlier
     // This is the only failure case for a request under 4996 bytes
     // currindex+4=index of memory start, memRequest+4=amount of memory requested+header size
-    if(currindex+4+(memReqShort+4)>5000){
-        printf("\nOut of memory error at %s, line %d",filename, line);
+    if(currindex+2+(memReqShort+2)>2500){
+        printf("\nOut of memory error at %s, line %d\n",filename, line);
         return NULL;
     }
- 
+
     // Case: valid request, no blocks after this point in simulated memory. (if it exceeded memory, would have returned above)
     // myblock[currindex+2] is the amount of memory in this block, myblock[currindex] is the next pointer
-    if(myblock[currindex]==0){
+    if(myShortBlock[currindex]==0 && myShortBlock[currindex+1]!=0){
         // sets current next pointer to refer to new block
-        nextindex=currindex+4+(unsigned short) myblock[currindex+2];
-        myblock[currindex]=nextindex;
+        nextindex=currindex+2+myShortBlock[currindex+1];
+        myShortBlock[currindex]=nextindex;
+        if(currindex==nextindex){
+            printf("frogged up line 78");
+        }
         // next field in new block is set to 0 to indicate that it is the last one
-        myblock[nextindex]=0;
-        myblock[nextindex+2]=memReqShort;
-        return (void*) &myblock[nextindex+4];
+        myShortBlock[nextindex]=0;
+        myShortBlock[nextindex+1]=memReqShort;
+        return (void*) &myShortBlock[nextindex+2];
     }
- 
+    //edge case: first real node
+    if(myShortBlock[currindex]==0 && myShortBlock[currindex+1]==0){
+        myShortBlock[currindex+1]=memReqShort;
+        return (void*) &myShortBlock[currindex+2];
+    }
+
     // Case: valid request (no need for conditional as alternatives already returned)
-    nextindex=currindex+4+myblock[currindex+2];
+    nextindex=currindex+2+myShortBlock[currindex+1];
+    if(myShortBlock[currindex]==myShortBlock[nextindex] && myShortBlock[nextindex]!=0){
+        printf("error line 96");
+    }
     // sets next field for new block
-    myblock[nextindex]=myblock[currindex];
+    myShortBlock[nextindex]=myShortBlock[currindex];
     // sets memory field for new block
-    myblock[nextindex+2]=(unsigned short) memReqShort;
-    myblock[currindex]=nextindex;
-   
-    return (void*) &myblock[nextindex+4];
+    myShortBlock[nextindex+1]=memReqShort; 
+    myShortBlock[currindex]=nextindex;
+    if(currindex==nextindex && currindex!=0){
+        printf("error line 102");
+    }
+    return (void*) &myShortBlock[nextindex+2];
 }
- 
+
 // improved version of free that prevents some user errors (freeing pointers that were not allocated and freeing non-pointers)
 void myfree(void* memptr, char* filename, int line){
+    unsigned short* myShortBlock=(unsigned short*) &myblock[0];
     long memptrVal=(long) memptr;
-    long memStart=(long) &myblock[0];
-    long freeIndex = memptrVal-memStart;
+    long memStart=(long) &myShortBlock[0];
+    unsigned short freeIndex = (memptrVal-memStart)/2;
     unsigned short index=0;
     unsigned short prev=0;
- 
+
     // Case: not a valid pointer to our dynamic memory
-    if(freeIndex<0 || freeIndex>5000){
-        printf("Attempts to free a type other than pointer at %s line %d",filename, line);
+    if(freeIndex<0 || freeIndex>2500){
+        printf("Attempts to free a type other than pointer at %s line %d\n",filename, line);
         return;
     }
- 
+
     // iterates through to find the correct chunk of memory
-    while(freeIndex>(unsigned short) myblock[index] && freeIndex!=index+4 && (unsigned short) myblock[prev]!=0){
+    while(freeIndex>myShortBlock[index] && freeIndex!=index+2 &&myShortBlock[prev]!=0){
         prev=index;
-        index=myblock[index];      
+        index=myShortBlock[index];      
     }
- 
+
     // Case: pointer was an address between two valid pointers (index has passed the pointer). Not a valid operation
-    if(index>freeIndex || myblock[prev]==0){
-        printf("Attempts to free invalid pointer at %s line %d",filename, line);
+    if(index>freeIndex || myShortBlock[prev]==0){
+        printf("Attempts to free invalid pointer at %s line %d\n",filename, line);
         return;
     }
- 
-    if(freeIndex==index+4){
+
+    if(freeIndex==index+2){
+        //case: delete head, nothing else is in the list
+        if(prev==0 && myShortBlock[index]==0){
+            myShortBlock[index]=0;
+            myShortBlock[index+1]=0;
+            myShortBlock[0]=2;
+            return;
+        }
         // effectively previous.next=current.next
-        myblock[prev]=(unsigned short) myblock[index];
- 
+        if(myShortBlock[prev]==myShortBlock[index] && myShortBlock[index]!=0){
+            printf("you goofed");
+        }
+        myShortBlock[prev]=myShortBlock[index];
+        myShortBlock[index]=0;
+        myShortBlock[index+1]=0;
         // Could set the other indices to 0, but not actually necessary since they are no longer recognized as pointers
-        // myblock[index]=(short) 0;
-        // myblock[index+2]=(short) 0;
     }
 }
+
+
+
+
