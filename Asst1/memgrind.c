b76bc5812cc4eb8660a84850704aac2ce75808d4
Nicholas Harras <nickharras1@gmail.com>
2018-02-18 01:59:34 -0500

    Attempting to assign values withing the scope of myblock[]... some MAJOR bugs

--- Asst1/memgrind.c
+++ Asst1/memgrind.c
@@ -40,10 +40,12 @@ int main(int argc, char* argv[]){
 	// C
 	gettimeofday(&start, NULL);
 	int count = 0;
-	for(i = 0; i < 150; i++){
+	int malloc_count = 0;
+	while(malloc_count < 150){
 		int r = rand()%2;
 		if(r == 0){
 			ptrs[count++] = mymalloc(1, __FILE__, __LINE__);
+			malloc_count++;
 		}
 		else  if(r == 1 && count < 0){
 			myfree(ptrs[count--], __FILE__, __LINE__);
@@ -54,7 +56,36 @@ int main(int argc, char* argv[]){
 	} // will this work idk lol
 	gettimeofday(&end, NULL);
 	elapsed = (end.tv_sec - start.tv_sec) * 1000.0;
-	elapsed += (end.tv_usec - start.tv_usec) / 1000.0; 
 	printf("Test C...time elapsed:\t%fms\n", elapsed);
+
+
+	// E
+	gettimeofday(&start, NULL);
+	int * int_ptrs[100];
+	i = 0;
+	while(i < 100){
+		int_ptrs[i] = mymalloc(4, __FILE__, __LINE__);
+		i++;
+	}
+	i = 0;
+	while(i < 100){
+		int r = rand()%100+1;
+		int_ptrs[i] = &r;
+		i++;
+	}
+	i = 0;
+	while(i < 100){ // To be commented out in final version, an option to test things
+		printf("value at ptrs[%d]:\t%d\n", i, *int_ptrs[i]);
+		i++;	
+	}
+	i = 0;
+	while(i < 100){
+		myfree(int_ptrs[i], __FILE__, __LINE__);
+		i++;
+	}
+	gettimeofday(&end, NULL);
+	elapsed = (end.tv_sec - start.tv_sec) * 1000.0;
+	printf("Test C...time elapsed:\t%fms\n", elapsed);
+
 	return 0;
 }
